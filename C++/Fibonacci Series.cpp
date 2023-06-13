780e7cb1478dde615a4643d0c362f4829c002815
Avnish Kumar <108517108+4ortyseve7@users.noreply.github.com>
2023-06-13 23:32:35 +0530

    coded fibonacci using for loop, values starting from 2 onwards.

--- C++/Fibonacci Series.cpp
+++ C++/Fibonacci Series.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    
+    cout << "Enter the number (>2) of Fibonacci sequence digits to print : ";
+    cin >> n;
+    
+    cout << "0 1 ";
+    for(int i = 0, j = 1, temp = 0, loop = 0; loop < n-2; loop++){
+        temp = i + j;
+        i = j;
+        j = temp;
+        cout << temp << " ";
+    }
+  
+  return(0);
+}
 
